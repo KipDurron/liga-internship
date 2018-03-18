dce88ec979923c54f35d0d07f85a434837e2476e
go910 <go910@yandex.ru>
2018-03-18 23:48:32 +0300

    for revert

--- javacore-song-task/src/main/java/ru/liga/App.java
+++ javacore-song-task/src/main/java/ru/liga/App.java
@@ -10,8 +10,10 @@ import ru.liga.HW.input_interface.InLoggers;
 import ru.liga.HW.input_interface.TextAnalizeFile;
 import ru.liga.HW.input_interface.TransMidi;
 import ru.liga.songtask.content.Content;
+import ru.liga.songtask.domain.Note;
 import ru.liga.songtask.domain.SimpleMidiFile;
 
+<<<<<<< HEAD
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -21,6 +23,12 @@ import java.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //////
+=======
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+>>>>>>> parent of 488ab80... Revert "notes"
 
 /**
  * Всего нот: 15
@@ -52,6 +60,7 @@ public class App {
     public static void main(String[] args) {
 
         SimpleMidiFile simpleMidiFile = new SimpleMidiFile(Content.ZOMBIE);
+<<<<<<< HEAD
 
     if (args.length==3)
     {
@@ -84,5 +93,72 @@ public class App {
 
 
 
+=======
+        System.out.println("Всего нот: " + simpleMidiFile.vocalNoteList().size());
+        System.out.println("Длительность (сек): " + simpleMidiFile.durationMs() / 1000);
+        System.out.println("<p>\nАнализ диапазона:");
+        List<Note> notes = simpleMidiFile.vocalNoteList();
+        Note minNote = notes.get(0);
+        Note maxNote = notes.get(0);
+        for(Note note : notes){
+            if(note.sign().higher(maxNote.sign())){
+                maxNote = note;
+            }
+            if(note.sign().lower(minNote.sign())){
+                minNote = note;
+            }
+        }
+        System.out.println("верхняя: " + maxNote.sign().fullName());
+        System.out.println("нижняя: " + minNote.sign().fullName());
+        System.out.println("диапазон: " + maxNote.sign().diffInSemitones(minNote.sign()) + "\n<p>");
+
+        System.out.println("Анализ длительности нот (мс):");
+        Map<Long, Integer> durations = new HashMap<>();
+        for(Note note : notes){
+            if(durations.containsKey(note.durationTicks())){
+                int temp = durations.get(note.durationTicks());
+                durations.put(note.durationTicks(), ++temp);
+            }
+            else {
+                durations.put(note.durationTicks(), 1);
+            }
+        }
+        for (Map.Entry<Long, Integer> entry : durations.entrySet())
+        {
+            System.out.println((int)(entry.getKey() * simpleMidiFile.tickInMs()) + ": " + entry.getValue());
+        }
+
+        System.out.println("<p>\nАнализ нот по высоте:");
+        Map<String, Integer> heights = new HashMap<>();
+        for(Note note : notes){
+            if(heights.containsKey(note.sign().fullName())){
+                int temp = heights.get(note.sign().fullName());
+                heights.put(note.sign().fullName(), ++temp);
+            }
+            else {
+                heights.put(note.sign().fullName(), 1);
+            }
+        }
+        for (Map.Entry<String, Integer> entry : heights.entrySet())
+        {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        System.out.println("<p>\nАнализ интервалов:");
+        Map<Integer, Integer> intervals = new HashMap<>();
+        for(int i = 0; i < notes.size() - 1; i++) {
+            int interval = notes.get(i).sign().diffInSemitones(notes.get(i + 1).sign());
+            if (intervals.containsKey(interval)) {
+                int temp = intervals.get(interval);
+                intervals.put(interval, ++temp);
+            } else {
+                intervals.put(interval, 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : intervals.entrySet())
+        {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+>>>>>>> parent of 488ab80... Revert "notes"
     }
 }
